d575fd6dae63a38e9c1a9bf382952214b6b32ee0
mardochee mawete <mardocheemawete@gmail>
2023-08-16 00:56:17 +0100

    all display

--- script.js
+++ script.js
@@ -1,3 +1,137 @@
-{ calculate } import './calculator';
-
 // TODO: Faire la manipulation du DOM dans ce fichier
+const currentInput = document.getElementById('input');
+currentInput.setAttribute('readonly', 'readonly');
+currentInput.placeholder = '0';
+const historical = document.getElementById('calcul');
+const numpadButton = document.querySelectorAll('.numpad');
+const groupOperateur = document.querySelectorAll('[type="submit"]');
+const reset = document.getElementById('reset');
+const clear = document.getElementById('clear');
+const changeSign = document.getElementById('plusoumoins');
+const percentage = document.getElementById('percentage');
+const equals = document.getElementById('equals');
+
+let buttonOperationIsPressed = false;
+let defaultLengthHistorical = undefined;
+let stringCalcul = '';
+let answer = '';
+
+//display current input
+function displayCurrentInput(buttonInput) {
+    if (buttonOperationIsPressed == true || historical.innerText.includes('=') == true) {
+        currentInput.value = buttonInput;
+        dynamicHistorical();
+        buttonOperationIsPressed = false;
+    }
+    else if (buttonInput == '0') {
+        if (currentInput.value != '0') {
+            currentInput.value += buttonInput;
+            dynamicHistorical();
+        }
+    }
+    else if (buttonInput == '.') {
+        if (currentInput.value.includes('.') == false) {
+            currentInput.value += buttonInput;
+            dynamicHistorical();
+        }
+    } else {
+        if (currentInput.value == '0' && buttonInput != '.') {
+            currentInput.value = buttonInput;
+            dynamicHistorical();
+        }
+        else {
+            currentInput.value += buttonInput;
+            dynamicHistorical();
+        }
+    }
+}
+//display dynamique historique
+function dynamicHistorical(){
+    if(historical.innerText !=''){
+        historical.innerText = historical.innerText.slice(0, defaultLengthHistorical) +' '+currentInput.value;
+    }
+}
+// numpd Buttons calculator
+numpadButton.forEach((bt) => {
+    bt.addEventListener('click', (e) => {
+        displayCurrentInput(e.target.textContent);
+    })
+})
+//keyboard button input
+document.addEventListener('keydown', (event) => {
+    let buttonKeyboard = event.key;
+    if (buttonKeyboard == '.') {
+        displayCurrentInput(buttonKeyboard);
+    } else if ((buttonKeyboard == '+' || buttonKeyboard == '-') || (buttonKeyboard == '/' || buttonKeyboard == '*')) {
+        if(buttonKeyboard == '/'){ displayStringCalcul(' ÷ '); }
+        else if(buttonKeyboard == '*'){ displayStringCalcul(' × '); }
+        else{ displayStringCalcul(' '+buttonKeyboard+' '); }  
+    }
+    else {
+        for (let i = 0; i <= 9; i++) {
+            if (buttonKeyboard == i) {
+                displayCurrentInput(buttonKeyboard);
+            }
+        }
+    }
+});
+// historical calcul string
+function displayStringCalcul(operator) {
+    if(historical.innerText.includes('=') == true){  historical.innerText = historical.innerText.split('=').join('');}
+    if (historical.innerText == '') {
+        historical.innerText = currentInput.value + operator;
+        buttonOperationIsPressed = true; // button operator is pressed
+        defaultLengthHistorical = historical.innerText.length; // get length historical
+    }
+    else if (buttonOperationIsPressed == true) {  //control sign
+        if (historical.innerText.slice(-2) != operator) {
+            historical.innerText = historical.innerText.slice(0, -2) + operator;
+        }
+    }
+    else {
+        historical.innerText += ' '+ operator+' ';
+        buttonOperationIsPressed = true; // button operator is pressed
+        defaultLengthHistorical = historical.innerText.length; // get length historical
+    }
+}
+// buttons operators
+groupOperateur.forEach(function (operator) {
+    operator.type = 'button';
+    operator.addEventListener('click', () => {
+        if (currentInput.value != '') { // if current input is empty, no input 
+            if (operator.innerText == '÷') {
+                displayStringCalcul(' ÷ ');
+            }
+            else if (operator.innerText == '-') {
+                displayStringCalcul(' - ');
+            }
+            else if (operator.innerText == '×') {
+                displayStringCalcul(' × ');
+            }
+            else if (operator.innerText == '+') {
+                displayStringCalcul(' + ');
+            }
+        }
+    })
+})
+//calcul
+function calculate(){
+    stringCalcul = historical.innerText; // get string calcul
+    if(stringCalcul !=''){
+        for(let char of stringCalcul){
+            if(char == '÷'){ stringCalcul = stringCalcul.split('÷').join('/') ; }
+            else if( char == '×'){ stringCalcul = stringCalcul.split('×').join('*') ;}
+            else if( char == ' '){ stringCalcul = stringCalcul.split(' ').join('') ;}
+        }
+        if(isNaN(stringCalcul.slice(-1)) == true){ stringCalcul = stringCalcul.slice(0, -1); } // if the last char is not a number
+        answer = eval(stringCalcul)
+        if(answer == 'Infinity'){ return 'ERROR'; }
+        else{ return answer;}   
+    }
+}
+//equals
+equals.addEventListener('click',()=>{
+    if(isNaN(historical.innerText.slice(-1)) == true){ historical.innerText = historical.innerText.slice(0, -1); }
+    currentInput.value = calculate();
+    historical.innerText +=' =';
+})
