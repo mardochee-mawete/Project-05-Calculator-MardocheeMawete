ce726aecbb13d73d3bd7a6ff3021f806b885bddb
mardochee mawete <mardocheemawete@gmail>
2023-10-20 00:19:18 +0200

    trigonometric Function Calculation

--- script.js
+++ script.js
@@ -17,25 +17,7 @@ let lastSign='';
 let answer = '';
 let buttonChangeSingIsPressed = false;
 
-//choose units (deg , rad, grad)
-const angleUnit = document.createElement('span');
-angleUnit.textContent = 'Deg';
-angleUnit.style.color='#f3f3f3';
-angleUnit.style.backgroundColor = 'rgba(0,0,0,0.3)';
-angleUnit.style.padding = '8px 0';
-angleUnit.style.width = '50px';
-angleUnit.style.textAlign = 'center';
-angleUnit.style.borderRadius = '10px';
-angleUnit.style.marginBottom = '-20px';
-angleUnit.style.cursor='pointer';
-document.querySelector('form').insertBefore(angleUnit, document.querySelector('form').children[0]);
-
-angleUnit.addEventListener('click', (e)=>{
-  if(e.target.textContent == 'Deg'){ angleUnit.textContent = 'Rad';}
-  else if(angleUnit.textContent == 'Rad'){ angleUnit.textContent = 'Grad';}
-  else if(angleUnit.textContent == 'Grad'){ angleUnit.textContent = 'Deg';}
-})
-//create the function and constant buttons
+//create container the function and constant buttons
 const containerButtons = document.querySelector('.buttons');
 const divButton = document.createElement('div');
 divButton.style.gridColumn = '1/4';
@@ -46,6 +28,7 @@ divButton.style.gridTemplateColumns = 'repeat(7, 1fr)';
 divButton.style.gap = '3px';
 containerButtons.insertBefore(divButton, containerButtons.children[4]);
 
+//create the function and constant buttons
 function createFunctionAndConstantButton(text){
   const button = document.createElement('button');
   button.className = 'numpad';
@@ -66,6 +49,65 @@ createFunctionAndConstantButton('log');
 createFunctionAndConstantButton('ln');
 createFunctionAndConstantButton('sin');
 createFunctionAndConstantButton('cos');
+
+//button unit (deg , rad, grad)
+const angleUnit = document.createElement('span');
+angleUnit.textContent = 'Deg';
+angleUnit.style.color='#f3f3f3';
+angleUnit.style.backgroundColor = 'rgba(0,0,0,0.3)';
+angleUnit.style.padding = '8px 0';
+angleUnit.style.width = '50px';
+angleUnit.style.textAlign = 'center';
+angleUnit.style.borderRadius = '10px';
+angleUnit.style.marginBottom = '-20px';
+angleUnit.style.cursor='pointer';
+document.querySelector('form').insertBefore(angleUnit, document.querySelector('form').children[0]);
+
+angleUnit.addEventListener('click', (e)=>{
+  if(e.target.textContent == 'Deg'){ angleUnit.textContent = 'Rad';}
+  else if(angleUnit.textContent == 'Rad'){ angleUnit.textContent = 'Grad';}
+  else if(angleUnit.textContent == 'Grad'){ angleUnit.textContent = 'Deg';}
+})
+
+// unit conversion
+function unitConversion(){
+  if(angleUnit.textContent == 'Deg'){ return parseFloat(currentInput.value) * (Math.PI / 180);}
+  else if(angleUnit.textContent == 'Grad'){ return (parseFloat(currentInput.value) * (Math.PI / 180)) / 1.1111}
+  else{ return currentInput.value;}
+}
+
+// calculation cos and sin
+function trigonometricFunctionCalculation(buttonTrigo){
+    if(isNaN(currentInput.value)==false && currentInput.value !=''){
+     if(buttonTrigo =='cos'){
+      if(currentInput.value == '90' || currentInput.value == '270'){ currentInput.value = '0'; }
+      else if(currentInput.value == '-90' || currentInput.value == '-270'){ currentInput.value = '0'; }
+      else{ currentInput.value = Math.cos(unitConversion()); }
+     }
+     else if(buttonTrigo =='sin'){
+      if(currentInput.value == '180' || currentInput.value == '180'){ currentInput.value = '0'; }
+      else if(currentInput.value == '270'){ currentInput.value = '-1'; }
+      else if(currentInput.value == '-270'){ currentInput.value = '1'; }
+      else{ currentInput.value = Math.sin(unitConversion()); }
+     }
+    }
+}
+
+// calculation log ln exp
+function exponentialAndLogarithmicFunctions(Log_exp_Ln){
+  if(isNaN(currentInput.value)==false && currentInput.value !=''){
+    if(Log_exp_Ln =='log'){
+      currentInput.value = Math.log10(currentInput.value);
+    }
+    else if(Log_exp_Ln =='exp'){
+      currentInput.value = Math.exp(currentInput.value);
+    }
+    else if(Log_exp_Ln =='ln'){
+      currentInput.value = Math.log(currentInput.value);
+    }
+  }
+}
+
 // create button 00
 const divButtonDoubleZero = document.createElement('div');
 const buttonDoubleZero = document.createElement('button');
@@ -74,6 +116,7 @@ buttonDoubleZero.type='button';
 buttonDoubleZero.textContent = '00';
 divButtonDoubleZero.appendChild(buttonDoubleZero);
 containerButtons.insertBefore(divButtonDoubleZero, containerButtons.children[19]);
+
 //create container historical
 const containerHistorical = document.createElement('div');
 containerHistorical.style.width = '22%';
@@ -89,13 +132,15 @@ titleHistorical.style.fontSize = '18px';
 titleHistorical.style.marginBottom='20px';
 containerHistorical.appendChild(titleHistorical);
 
+// change title Historique
 function changeTextTitleHistorical(){
   if(containerHistorical.children[1]){
     titleHistorical.textContent = "Historique";
   }
 }
+
 //set calcul in historical
-function setCalculInHistorical(){
+function sendCalculationToHistorical(){
   const stringHistorical = document.createElement('p');
   stringHistorical.textContent = stringCalcul.innerText +' '+ currentInput.value;
   stringHistorical.style.fontSize = '16px';
@@ -103,23 +148,28 @@ function setCalculInHistorical(){
   stringHistorical.style.color="#333";
   stringHistorical.style.cursor = 'pointer';
   containerHistorical.appendChild(stringHistorical);
-  //cover a past calculation
-  stringHistorical.addEventListener('click',(e) => {
+ coverPastCalculation(stringHistorical);
+}
+
+ //cover a past calculation
+ function coverPastCalculation(calculHistorical){
+  calculHistorical.addEventListener('click',(e) => {
     let indexSignEquals = e.target.textContent.indexOf('=');
     stringCalcul.innerText = e.target.textContent.slice(0 , indexSignEquals + 1);
     currentInput.value = e.target.textContent.slice(indexSignEquals + 1);
     e.target.style.color = '#ff0000';
     e.target.style.textDecoration ='underline';
   })
-  stringHistorical.addEventListener('mouseover',(e) => {
+  calculHistorical.addEventListener('mouseover',(e) => {
     e.target.style.color = '#0000ff';
     e.target.style.textDecoration ='underline';
   })
-  stringHistorical.addEventListener('mouseleave',(e) => {
+  calculHistorical.addEventListener('mouseleave',(e) => {
     e.target.style.color = '#333';
     e.target.style.textDecoration ='none';
   })
-}
+ }
+
 // numpd Buttons calculator
 document.querySelectorAll('.numpad').forEach((bt) => {
   bt.addEventListener('click', (e) => {
@@ -132,9 +182,10 @@ document.addEventListener('keydown', (event) => {
   if (/[0-9.+-/*//]/.test(key)) {
     key.replace("*", ' × ').replace("/", ' ÷ ');
     if (/[0-9.]/.test(key)) displayCurrentInput(key);
-    else displayStringCalcul(key);
+    else displayCalculationString(key);
   }
 });
+
 //display current input
 function displayCurrentInput(buttonInput) {
   if ((isNaN(stringCalcul.innerText.slice(-1)) == true && stringCalcul.innerText.slice(-1) != '.')) {
@@ -143,84 +194,69 @@ function displayCurrentInput(buttonInput) {
   if (buttonInput == '0') {
     if (currentInput.value != '0' && currentInput.value != '-0') {
       currentInput.value += buttonInput;
-      dynamicStringCalcul();
+      dynamicDisplayCalculationString();
     }
   }
   else if (buttonInput == '00') {
     if ((currentInput.value != '' && currentInput.value != '-') && (currentInput.value != '0' && currentInput.value != '-0')) {
       currentInput.value += buttonInput;
-      dynamicStringCalcul();
+      dynamicDisplayCalculationString();
     }
   } 
   else if (buttonInput == '.') {
     if (currentInput.value.includes('.') == false) {
-      if(currentInput.value == ''){
-        currentInput.value = '0.';
-        dynamicStringCalcul();
-      }else{
-        currentInput.value += buttonInput;
-        dynamicStringCalcul();
-      }
+      if(currentInput.value == ''){ currentInput.value = '0.'; }
+      else{ currentInput.value += buttonInput; }
+      dynamicDisplayCalculationString();
     }
   }
   else if(buttonInput == 'π'){
     currentInput.value = Math.PI;
-    dynamicStringCalcul();
+    dynamicDisplayCalculationString();
   }
   else if(buttonInput == 'e'){
     currentInput.value = Math.E;
-    dynamicStringCalcul();
+    dynamicDisplayCalculationString();
   }
-  else if((buttonInput =='cos'|| buttonInput =='sin') || ((buttonInput =='log'|| buttonInput =='ln' )|| buttonInput =='exp')){
-    if(isNaN(currentInput.value)==false && currentInput.value !=''){
-     if(buttonInput =='cos'){
-      currentInput.value = Math.cos(currentInput.value);
-     }
-     else if(buttonInput =='sin'){
-      currentInput.value = Math.sin(currentInput.value);
-     }
-     else if(buttonInput =='log'){
-      currentInput.value = Math.log10(currentInput.value);
-     }
-     else if(buttonInput =='exp'){
-      currentInput.value = Math.exp(currentInput.value);
-     }
-     else if(buttonInput =='ln'){
-      currentInput.value = Math.log(currentInput.value);
-     }
-    }
+  else if(buttonInput =='cos'|| buttonInput =='sin'){
+    trigonometricFunctionCalculation(buttonInput);
+  }
+  else if((buttonInput =='log'|| buttonInput =='ln' ) || buttonInput =='exp'){
+    exponentialAndLogarithmicFunctions(buttonInput);
   }
   else {
     if (currentInput.value == '0' && buttonInput != '.') {
       currentInput.value = buttonInput;
-      dynamicStringCalcul();
+      dynamicDisplayCalculationString();
     }
     else {
       currentInput.value += buttonInput;
-      dynamicStringCalcul();
+      dynamicDisplayCalculationString();
     }
   }
 }
+
 // buttons operators
 groupOperateur.forEach(function(operator) {
   operator.type = 'button';
   operator.addEventListener('click', () => {
     if (operator.textContent == '÷') {
-      displayStringCalcul(' ÷ ');
+      displayCalculationString(' ÷ ');
     }
     else if (operator.textContent == '-') {
-      displayStringCalcul(' - ');
+      displayCalculationString(' - ');
     }
     else if (operator.textContent == '×') {
-      displayStringCalcul(' × ');
+      displayCalculationString(' × ');
     }
     else if (operator.textContent == '+') {
-      displayStringCalcul(' + ');
+      displayCalculationString(' + ');
     }
   })
 })
+
 // display calcul string
-function displayStringCalcul(operator) {
+function displayCalculationString(operator) {
   if (stringCalcul.innerText.slice(-1) == '=') {
     stringCalcul.innerText = currentInput.value; // set final answer
     currentInput.value = '';
@@ -246,12 +282,14 @@ function displayStringCalcul(operator) {
     lastSign = operator;
   }
 }
+
 //display dynamic string calcul
-function dynamicStringCalcul() {
+function dynamicDisplayCalculationString() {
   if (stringCalcul.innerText != '') {
     stringCalcul.innerText = stringCalcul.innerText.slice(0, lengthStringCalcul) + ' ' + currentInput.value;
   }
 }
+
 //calcul
 function calculate() {
   if (stringCalcul.innerText != '') {
@@ -260,6 +298,7 @@ function calculate() {
     else { return answer; }
   }
 }
+
 //equals
 equals.addEventListener('click', () => {
   if (stringCalcul.innerText != '') {
@@ -267,15 +306,15 @@ equals.addEventListener('click', () => {
       lastInput = currentInput.value;
       currentInput.value = calculate();
       stringCalcul.innerText += ' =';
-      setCalculInHistorical();
-      changeTextTitleHistorical()
+      sendCalculationToHistorical();
+      changeTextTitleHistorical();
     }
     else if(stringCalcul.innerText.includes('=') == true){
       stringCalcul.innerText = currentInput.value + lastSign + lastInput;
       currentInput.value = calculate();
       stringCalcul.innerText += ' =';
-      setCalculInHistorical();
-      changeTextTitleHistorical()
+      sendCalculationToHistorical();
+      changeTextTitleHistorical();
     }
     else {
       if (isNaN(parseFloat(currentInput.value)) == false) {
@@ -289,17 +328,18 @@ equals.addEventListener('click', () => {
           lastInput = currentInput.value;
           currentInput.value = calculate();
           stringCalcul.innerText += ' =';
-          setCalculInHistorical();
+          sendCalculationToHistorical();
           changeTextTitleHistorical()
       }
     }
   }
 })
+
 //percentage
 percentage.addEventListener('click', () => {
   if (stringCalcul.innerText != '') {
     if (isNaN(stringCalcul.innerText.slice(-1)) == true) {
-      if (stringCalcul.innerText.slice(-1) == '=') { // after button = is pressi
+      if (stringCalcul.innerText.slice(-1) == '=') { // after button = is pressed
         stringCalcul.innerText = stringCalcul.innerText.slice(0, -1);
         currentInput.value = calculate() / 100;
         stringCalcul.innerText = '';
@@ -315,6 +355,7 @@ percentage.addEventListener('click', () => {
     }
   }
 })
+
 //reset
 reset.addEventListener('click', () => {
   lengthStringCalcul = undefined;
@@ -328,8 +369,9 @@ reset.addEventListener('click', () => {
 //clear
 clear.addEventListener('click', () => {
   currentInput.value = '';
-  dynamicStringCalcul();
+  dynamicDisplayCalculationString();
 })
+
 //change Sign current input
 changeSign.addEventListener('click', () => {
   if (isNaN(parseFloat(currentInput.value)) == false) {
